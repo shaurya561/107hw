a7d34e73ed44ae70b4f103bee113a90cda82dd3d
shaurya561 <77106149+shaurya561@users.noreply.github.com>
2021-06-10 18:46:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5version",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zHL1IIdyE/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("model is loaded");
+}
+
+function speak() {
+    var synth=window.speechSynthesis;
+    speak_data_one="The first prediction is"+prediction_1;
+    speak_data_two="The second prediction is"+prediction_2;
+    var utterthis=new SpeechSynthesisUtterance("speak_data_one+speak_data_two");
+    synth.speak(utterthis);
+}
+
